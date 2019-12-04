588defc97a23546d71a36f24cdc82c21d7794eb4
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2019-12-04 14:34:50 +0530

    Styling: Styled 'Header' (Inline)

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,15 @@ import React, {Component} from 'react';
 
 class Header extends Component{
     render(){
+        const headerStyle = {
+            textAlign: 'center',
+            padding: 20,
+            background: '#000',
+            color: '#fff',
+            textTransform: 'uppercase'
+        };
         return(
-            <div className = "header">
+            <div className = {headerStyle}>
                 {this.props.title} 
             </div>
         );
