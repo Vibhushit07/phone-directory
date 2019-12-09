bfb804f6fa4f55accef6e98cdcf4359fb77f1f8a
Vibhushit Singhal <vibhushitsinghal80@gmail.com>
2019-12-09 15:22:02 +0530

    Updates

---
+++ precache-manifest.47b48828a9fbb41a9a0f265faea837e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9fde1806eef2108316529f798d3b24",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "f516c28f65de2f762b72",
+    "url": "/phone-directory/static/css/main.a0f52ae0.chunk.css"
+  },
+  {
+    "revision": "977154775b3dd6e22f5e",
+    "url": "/phone-directory/static/js/2.ce975f0f.chunk.js"
+  },
+  {
+    "revision": "f516c28f65de2f762b72",
+    "url": "/phone-directory/static/js/main.281e61d1.chunk.js"
+  },
+  {
+    "revision": "750e5236f65c3a1f5e4c",
+    "url": "/phone-directory/static/js/runtime-main.32348089.js"
+  }
+]);
